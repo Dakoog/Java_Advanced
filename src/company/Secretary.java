334aa05a30dda3cab2b3154a73bda686d5abc2a8
Dakoog <taktoja@spoko.pl>
2022-04-29 00:11:24 +0200

    Added output System.out.f about constructor args

--- src/company/Secretary.java
+++ src/company/Secretary.java
@@ -3,9 +3,11 @@ package company;
 public final class Secretary extends Employee{
     public Secretary(String name, int salary) {
         super(name, salary);
+        System.out.printf("Data from Secretary constructor. Name: %s, Salary: %s adn Bonus %s%n",name, salary);
     }
     public void makeCall(String number){
         System.out.println("I'm call to number "+ number);
+
     }
 
 }
