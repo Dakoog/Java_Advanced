f4a107907d7d17127642fc1619c149d752a1fc1d
Dakoog <taktoja@spoko.pl>
2022-04-29 22:47:55 +0200

    Modify class Manager - added field with object Secretary for composition to method makeCall.

--- src/company/Manager.java
+++ src/company/Manager.java
@@ -4,18 +4,33 @@ import java.math.BigDecimal;
 
 public class Manager extends Employee {
     private int bonus;
+    private Secretary secretary; // object Secretary for composition
 
-    public Manager(String name, int salary) {
-        super(name, salary);
+    public void setSecretary(Secretary secretary){ // added new secretary
+        this.secretary = secretary;
+    }
+
+    public void makeCall(String number){
+        if (this.secretary == null){
+            System.out.println("I cannot make call. I need secretary.");
+        } else {
+            this.secretary.makeCall(number);
+        }
+    }
+
+    public Manager(String name, int salary, int age) {
+        super(name, salary, age);
         System.out.printf("Data from Employee constructor. Name: %s, Salary: %s", name, salary);
     }
 
-    public Manager(String name, int salary, int bonus) {
-        super(name, salary);
-        System.out.printf("Data from Manager constructor. Name: %s, Salary: %s adn Bonus %s%n", name, salary, bonus);
+    public Manager(String name, int salary, int age, int bonus) {
+        super(name, salary, age);
+        this.bonus = bonus;
+        System.out.printf("Data from Manager constructor. Name: %s, Salary: %s  Bonus: %s Age: %s%n", name, salary, bonus, age);
 
     }
 
+    @Override
     public void show() { // on overridden method -> overrides a method in a base class and added method with base class (super.show());
         super.show();
         System.out.println("I am manager");
@@ -27,8 +42,8 @@ public class Manager extends Employee {
 
     @Override  // an overridden method -> overrides a method in a base class
     public int getSalary() {
+        super.getSalary();
         return salary + bonus;
-
     }
 
     public void makeSpeech(String additionalMessage) { // method overload with an additional argument
