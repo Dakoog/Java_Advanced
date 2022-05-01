0ac58129daa9e7c916ef179bbe7b1c0006742a9a
Dakoog <taktoja@spoko.pl>
2022-05-01 17:09:21 +0200

    added default method count() for Runner interface.

--- src/animals/Runner.java
+++ src/animals/Runner.java
@@ -1,18 +1,15 @@
 package animals;
 
 public interface Runner {
-  /* public abstract - default*/ void run();
-  // all methods for interface are public and abstract
+    /* public abstract - default*/ void run();
+    // all methods for interface are public and abstract
 
     // the interface method that the implementation can use is only: static, default, or private
-    static void fastRun(){
+    static void fastRun() {
         System.out.println("I can run even faster");
     }
-    private void fasterRunner(){
-        System.out.println("Pshouuuuuuuuuuup.");
-    }
-    default  void fastRunner(){
-        System.out.println("I am faster runner");
-    }
 
+    default void count() {
+
+    }
 }
