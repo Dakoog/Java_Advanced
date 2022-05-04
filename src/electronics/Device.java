3d21a41f084b7276e85c6c12f5ea17c30957812c
Dakoog <taktoja@spoko.pl>
2022-05-04 18:51:46 +0200

    Created new package with abstract class "Device"

---
+++ src/electronics/Device.java
@@ -0,0 +1,15 @@
+package electronics;
+
+public abstract class Device {
+    private String model;
+
+    public Device(String model) {
+        this.model = model;
+    }
+    public abstract void turnOn();
+    public abstract void turnOff();
+
+
+
+
+}
