f7a4f01686d7307dcd5d06d97e09769b67fa0f04
Dakoog <taktoja@spoko.pl>
2022-05-04 18:40:42 +0200

    Modify interface Runner

--- src/animals/Runner.java
+++ src/animals/Runner.java
@@ -4,11 +4,6 @@ public interface Runner {
     /* public abstract - default*/ void run();
     // all methods for interface are public and abstract
 
-    // the interface method that the implementation can use is only: static, default, or private
-    static void fastRun() {
-        System.out.println("I can run even faster");
-    }
-
     default void count() {
 
     }
