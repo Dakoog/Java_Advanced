963a4c5d0eea6efce951ee5a60811e028ca1000c
Dakoog <taktoja@spoko.pl>
2022-05-06 20:22:29 +0200

    Adding new class Tablet which extends abstract class Device of package electronics

--- src/electronics/Tablet.java
+++ src/electronics/Tablet.java
@@ -1,4 +1,21 @@
 package electronics;
 
-public class Tablet {
+public class Tablet extends Device{
+
+    public Tablet(String model) {
+        super(model);
+    }
+    public void sendMessage(String type, String message){
+
+    }
+
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
 }
